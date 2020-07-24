1eb7b636b91a4f28ae9788fbce66a1b59d2b618b
Виталий Ивановский <vivanovskijj@gmail.com>
2020-07-24 12:24:34 +0300

    add tk19.py (review ttkthemes)

---
+++ tk19.py
@@ -0,0 +1,12 @@
+# from tkinter import *
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+
+root = ThemedTk(theme='elegance')
+root.geometry('400x300+300+200')
+
+ttk.Button(root, text='Button').pack(pady=10)
+ttk.Entry(root).pack()
+
+root.mainloop()
